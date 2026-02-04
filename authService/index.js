9f5924e6e9b7a39e037d426835e1ece8150eab76
deep <dipvadukiya06@gmail.com>
2026-02-04 19:25:00 +0100

    rate limit: added IP rate limit in the auth service

--- authService/index.js
+++ authService/index.js
@@ -4,6 +4,15 @@ const dotenv = require("dotenv");
 const publicKeyRoute = require("./routes/auth/publicKeyRoute");
 const loginRoute = require("./routes/auth/loginRoute");
 const { correlationIdMiddleware } = require("../correlationId");
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 10,
+  message:
+    "Too many login attempts from this IP, please try again after a minute",
+  headers: true,
+});
 
 dotenv.config();
 
@@ -11,6 +20,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
+app.use(limiter);
 app.use(express.json());
 app.use(correlationIdMiddleware);
 
